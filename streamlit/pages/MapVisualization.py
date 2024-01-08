270fe15420b3f293fdbcdcbb5bf0bc3e75a252e2
dwang <dwang.ccny@gmail.com>
2024-01-08 01:51:52 -0500

    Update

--- streamlit/pages/MapVisualization.py
+++ streamlit/pages/MapVisualization.py
@@ -15,9 +15,13 @@ def load_data(zone,zip,collision):
 
     return zoneData, zipData, df
 
-zoningRaw = 'https://raw.githubusercontent.com/dwang312/NYC-Collision-Study/main/data/NYC-ZoningDistrict-Geodata.json'
-zipcodeRaw = 'https://raw.githubusercontent.com/dwang312/NYC-Collision-Study/main/data/NYC-ZipCode-Geodata.geojson'
-fp = 'https://media.githubusercontent.com/media/dwang312/NYC-Collision-Study/main/data/NYC-CollisionZonesWeather-Jun2012-Dec2023.csv'
+#zoningRaw = 'https://raw.githubusercontent.com/dwang312/NYC-Collision-Study/main/data/NYC-ZoningDistrict-Geodata.json'
+#zipcodeRaw = 'https://raw.githubusercontent.com/dwang312/NYC-Collision-Study/main/data/NYC-ZipCode-Geodata.geojson'
+#fp = 'https://media.githubusercontent.com/media/dwang312/NYC-Collision-Study/main/data/NYC-CollisionZonesWeather-Jun2012-Dec2023.csv'
+
+zoningRaw = 'NYC-Collision-Study\data\NYC-ZoningDistrict-Geodata.json'
+zipcodeRaw = 'NYC-Collision-Study\data\NYC-ZipCode-Geodata.geojson'
+fp = 'NYC-Collision-Study\data\NYC-CollisionZonesWeather-Jun2012-Dec2023.csv'
 
 mapInteractive = folium.Map(location=[40.71, -74.00], 
                       zoom_start=11, 
