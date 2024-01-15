5bb6761caf2a8c81f6f8b0d1889e9c7744deb451
dwang <dwang.ccny@gmail.com>
2024-01-14 22:26:08 -0500

    Zones

--- streamlit/pages/MapVisualization.py
+++ streamlit/pages/MapVisualization.py
@@ -23,25 +23,25 @@ def load_data(zone,zoneID,zip,collision):
     return zoneData, zoneID, zipData, df
 
 def load_ziplayer(mapInteractive,df,zipcodes):
-    # gb = df.groupby(['BOROUGH','ZIP CODE']).size().reset_index(name='Count')
+    gb = df.groupby(['BOROUGH','ZIP CODE']).size().reset_index(name='Count')
 
-    # # Get the total count for each borough
-    # borough_total = gb.groupby('BOROUGH')['Count'].sum().reset_index(name='BoroughTotal')
+    # Get the total count for each borough
+    borough_total = gb.groupby('BOROUGH')['Count'].sum().reset_index(name='BoroughTotal')
 
-    # # Merge the total count back to the grouped data
-    # gb = pd.merge(gb, borough_total, on='BOROUGH')
+    # Merge the total count back to the grouped data
+    gb = pd.merge(gb, borough_total, on='BOROUGH')
 
-    # # Calculate the percentage within each borough
-    # gb['Percentage Per Borough'] = (gb['Count'] / gb['BoroughTotal']) * 100
-    # gb['Percentage Per Borough'] = gb['Percentage Per Borough'].round(2)
+    # Calculate the percentage within each borough
+    gb['Percentage Per Borough'] = (gb['Count'] / gb['BoroughTotal']) * 100
+    gb['Percentage Per Borough'] = gb['Percentage Per Borough'].round(2)
 
-    # # Removing decimal and anything after
-    # gb['ZIP CODE'] = gb['ZIP CODE'].astype(str).str.split('.').str[0]
+    # Removing decimal and anything after
+    gb['ZIP CODE'] = gb['ZIP CODE'].astype(str).str.split('.').str[0]
 
-    # zip_gbd = gb.merge(zipcodes, left_on='ZIP CODE', right_on='ZIPCODE', how='left')
+    zip_gbd = gb.merge(zipcodes, left_on='ZIP CODE', right_on='ZIPCODE', how='left')
 
-    # zip_gbd.dropna(inplace=True)
-    # geo_zipdf = gpd.GeoDataFrame(zip_gbd, geometry='geometry', crs='EPSG:4326')
+    zip_gbd.dropna(inplace=True)
+    geo_zipdf = gpd.GeoDataFrame(zip_gbd, geometry='geometry', crs='EPSG:4326')
 
     style_function = lambda x: {'fillColor': '#ffffff', 
                                 'color':'#000000', 
@@ -52,28 +52,28 @@ def load_ziplayer(mapInteractive,df,zipcodes):
                                     'fillOpacity': 0.50, 
                                     'weight': 0.1}
 
-    # zip_gb = folium.features.GeoJson(
-    #     geo_zipdf,
-    #     style_function=style_function, 
-    #     control=False,
-    #     highlight_function=highlight_function, 
-    #     tooltip=folium.features.GeoJsonTooltip(
-    #         fields=['ZIP CODE', 'Percentage Per Borough'],
-    #         aliases=['Zip Code', 'Percentage Per Borough '],
-    #         style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;"),
-    #     )
-    # )
-    
     zip_gb = folium.features.GeoJson(
-        zipcodes,
+        geo_zipdf,
         style_function=style_function, 
         control=False,
         highlight_function=highlight_function, 
+        tooltip=folium.features.GeoJsonTooltip(
+            fields=['ZIP CODE', 'Percentage Per Borough'],
+            aliases=['Zip Code', 'Percentage Per Borough '],
+            style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;"),
+        )
     )
+    
+    # zip_gb = folium.features.GeoJson(
+    #     zipcodes,
+    #     style_function=style_function, 
+    #     control=False,
+    #     highlight_function=highlight_function, 
+    # )
 
     mapInteractive.add_child(zip_gb)
 
-def load_zoning(mapInteractive,zone, zoningID):
+def load_zoning(mapInteractive,zone,zoningID):
 
     def filterDist(dist):
         #If has a residential designation:
@@ -87,14 +87,25 @@ def load_zoning(mapInteractive,zone, zoningID):
         
     #Apply the filter to our dataframe to create a new column:
     zoningID['District Type'] = zoningID['Zoning District'].apply(filterDist)
-    folium.Choropleth(geo_data=zone,
-                  data=zoningID,
-                  columns=['arbID', 'District Type'],
-                  key_on='feature.properties.arbID',
-                  fill_color='YlOrRd',
-                  fill_opacity=0.7,
-                  line_opacity=0.3
-                  ).add_to(mapInteractive)
+
+    zone = folium.features.GeoJson(
+        zone,
+        style_function=lambda x: {'color':'#000000', 
+                                  'fillOpacity': 0.1, 
+                                  'weight': 0.1},
+        control=False,
+        highlight_function= lambda x: {'fillColor': '#000000', 
+                                    'color':'#000000', 
+                                    'fillOpacity': 0.50, 
+                                    'weight': 0.1},
+        tooltip=folium.features.GeoJsonTooltip(
+            fields=['ZONEDIST'],
+            style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;"),
+            aliases=['Zoning District: ']
+        )
+    )
+    mapInteractive.add_child(zone)
+
 
 #Load Data
 zoningRaw = config['paths']['zoning_raw']
@@ -110,7 +121,7 @@ mapInteractive = folium.Map(location=[40.71, -74.00],
 
 zone, zoningID, zipcodes, df = load_data(zoningRaw,zoningIDRaw,zipcodeRaw,fp)
 #load_ziplayer(mapInteractive,df,zipcodes)
-#load_zoning(mapInteractive,df,zone, zoningID)
+load_zoning(mapInteractive,zone,zoningID)
 
 folium.LayerControl().add_to(mapInteractive) 
 
